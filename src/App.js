89a10b318c871a411e5d9b0f45f3d6e90c034d8a
Syed Shameem <shahidclt93@gmail.com>
2023-10-31 00:33:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter , Route,  Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import productsData from './productsData';
-import reviewsData from './reviewData'
-import { BrowserRouter , Route,  Routes } from 'react-router-dom';
+import reviewsData from './reviewData';
+import Profile from './pagees/Profile';
+import SignIn from './pagees/SignIn';
+import SignUp from './pagees/SignUp';
+import About from './pagees/About';
 
 
 function App(){
@@ -17,6 +21,10 @@ function App(){
               <Route path="/" element={<Home productsData={productsData} reviewsData={reviewsData}/>} />
               <Route path="/login" element={<Login />} />
               <Route path="/cart" element={<Cart />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/sign-in" element={<SignIn />} />
+              <Route path="/about" element={<About />} />
               </Routes>
             <Footer/>  
         </BrowserRouter>
